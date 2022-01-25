4a933e61f8c5fcb1cb3ec93cd9b2fc39fe814e26
渡辺ユリ <yuriwatanabe9090@gmail.com>
2022-01-25 09:15:07 -0300

    add: add_notebook input parameters validations tests

--- src/domain/usecase/add_notebook.js
+++ src/domain/usecase/add_notebook.js
@@ -1,9 +1,18 @@
-function addNotebook({ notebookReporitory }, {
-  title,
-  description,
-  user
-}) {
-  console.log(title, description, user)
-}
-
-module.exports = addNotebook
+const addNotebookUseCase = {
+  create: ({ IdGenerator, Err, NotebookRepository }) => {
+    if (!Err) Err = Error;
+    if (!IdGenerator) throw new Err("An Id generator function should be specified");
+    return function ({ id, title, description, user } = {}) {
+      if (!id) id = IdGenerator(id);
+      if (!title) throw new Err("A title should be specified");
+      if (!user) throw new Err("An user should be specified");
+      return {
+        id,
+        title,
+        description,
+        user,
+      };
+    };
+  },
+};
+module.exports = addNotebookUseCase;
--- test/domain/usecase/add_notebook.test.js
+++ test/domain/usecase/add_notebook.test.js
@@ -1,10 +1,37 @@
-const addNotebook = require('../../../src/domain/usecase/add_notebook')
+const crypto = require('crypto');
+const addNotebookUseCase = require('../../../src/domain/usecase/add_notebook')
 
-test('add a new usecase', () => {
-  parameters = {
-    title: 'title',
-    description: '',
-    user: 'fulano'
-  }
-  addNotebook({ notebookReporitory: null }, parameters)
-})
+const addNotebook = addNotebookUseCase.create({
+  IdGenerator: () => crypto.randomUUID(),
+  Err: Error
+});
+
+describe('When Adding an notebook to system', () => {
+  it('It should be successfully when all parameters are ok', () => {
+    const notebook = addNotebook({
+      title: "title",
+      description: "",
+      user: 'fulano'
+    });
+    expect(notebook.id).not.toBeNull();
+    expect(notebook.title).toBe("title");
+    expect(notebook.description).toBe("");
+    expect(notebook.user).toBe("fulano");
+  })
+  it('It should thrown an exception when title is empty', () => {
+    const notebook = () => addNotebook({
+      title: "",
+      description: "",
+      user: 'fulano'
+    });
+    expect(notebook).toThrow(Error("A title should be specified"));
+  })
+  it('It should thrown an exception when user is null', () => {
+    const notebook = () => addNotebook({
+      title: "title",
+      description: "",
+      user: null
+    });
+    expect(notebook).toThrow(Error("An user should be specified"));
+  })
+});
