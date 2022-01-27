7f4ae9ad013168a2379d627ed9d4a9313fe5bff9
渡辺ユリ <yuriwatanabe9090@gmail.com>
2022-01-27 08:35:27 -0300

    add: notebook memory repository

---
+++ src/adapter/repository/notebook_repository_memory.js
@@ -0,0 +1,29 @@
+const notebookRepository = require("../../domain/repository/notebook_repository");
+let notebooks = [];
+
+function get({id}) {
+  return notebooks.find(notebook => notebook.id === id);
+}
+function update({notebook}) {
+  const oldNotebook = get({id:notebook.id});
+  if(!oldNotebook) return;
+  notebooks = notebooks.map(nb => {
+    if(nb.id === notebook.id) return notebook;
+    return nb;
+  });
+}
+
+function remove({id}) {
+  notebooks = notebooks.filter(notebook => id !== notebook.id);
+}
+
+function save({notebook}) {
+  notebooks.push(notebook);
+}
+
+module.exports = notebookRepository.create({
+  get,
+  update,
+  remove,
+  save
+});
